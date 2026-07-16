3944cd5ed00b06e60bc13380df92f27169d4b908
Kristian Skov <kris701kj@gmail.com>
2026-07-16 09:44:00 +0200

    Got the package flow working

--- projects/ez-ui-demo/src/app/pages/input/textInput.ts
+++ projects/ez-ui-demo/src/app/pages/input/textInput.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { EzUITextInput } from 'ez-ui';
+import { EzUITextInput } from 'EzUI';
 
 @Component({
     selector: 'app-text-input',
@@ -11,12 +11,31 @@ import { EzUITextInput } from 'ez-ui';
 	EzUITextInput
 ],
     template: `
-	<span>text</span>
-	<ezui-textinput/>
+	<span>Simple</span>
+	<ezui-textinput />
+
+	<span>Label</span>
+	<ezui-textinput label="Simple Label" />
+
+	<span>Label and Icon</span>
+	<ezui-textinput label="Label with icon" icon="square-pen" />
+
+	<span>Binding</span>
+	<ezui-textinput [(value)]="binding" label="Binding" />
+	<span>You wrote: {{binding()}}</span>
+
+	<span>Sizes</span>
+	<ezui-textinput label="Small" icon="square-pen" size="s"/>
+	<ezui-textinput label="Medium" icon="square-pen" size="m"/>
+	<ezui-textinput label="Large" icon="square-pen" size="l"/>
+
+	<span>Disabled</span>
+	<ezui-textinput label="Disabled" icon="square-pen" [disabled]="true"/>
     `,
     host: {
         class: 'base-view'
     }
 })
 export class TextInput {
+	binding = signal<string>("");
 }
