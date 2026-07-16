ab071b13dbf0e83cc09fc41465bc15dcfd87fac9
Kristian Skov <kris701kj@gmail.com>
2026-07-16 10:25:41 +0200

    Got the initial layout working

---
+++ projects/ez-ui-demo/src/app/common/samplecontainer.ts
@@ -0,0 +1,100 @@
+import { CommonModule } from '@angular/common';
+import { Component, ContentChild, Input, signal, TemplateRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TuiTabs, TuiTextarea } from '@taiga-ui/kit';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
+
+@Component({
+    selector: 'app-samplecontainer',
+    imports: [
+    FormsModule,
+    CommonModule,
+    TuiCardLarge,
+    TuiHeader,
+    TuiTabs,
+    TuiTextarea
+],
+    template: `
+	    <div appearance="floating" tuiCardLarge style="gap:0px">
+	        <header tuiHeader>
+	            <h3 tuiTitle>{{label}}</h3>
+	        </header>
+
+			<section>
+				<tui-tabs [(activeItemIndex)]="index" style="margin-bottom:10px;">
+					<button
+						iconStart="eye"
+						tuiTab
+						type="button"
+						(click)="index.set(0)"
+					>
+						Preview
+					</button>
+					<button
+						iconStart="code"
+						tuiTab
+						type="button"
+						(click)="index.set(1)"
+					>
+						HTML
+					</button>
+					<button
+						iconStart="code"
+						tuiTab
+						type="button"
+						[disabled]="!enableStyle"
+						(click)="index.set(2)"
+					>
+						CSS
+					</button>
+					<button
+						iconStart="code"
+						tuiTab
+						type="button"
+						[disabled]="!enableTypescript"
+						(click)="index.set(3)"
+					>
+						TS
+					</button>
+				</tui-tabs>
+
+				@switch (index()) {
+					@case (0) {
+						<div class="flex flex-col gap-2">
+							<ng-container #previewContent [ngTemplateOutlet]="preview"></ng-container>
+						</div>
+					}
+					@case (1) {
+						<tui-textfield>
+							<textarea tuiTextarea [disabled]="true" [value]="html"></textarea>
+						</tui-textfield>
+					}
+					@case (2) {
+						<tui-textfield>
+							<textarea tuiTextarea [disabled]="true" [value]="css"></textarea>
+						</tui-textfield>
+					}
+					@case (3) {
+						<tui-textfield>
+							<textarea tuiTextarea [disabled]="true" [value]="ts"></textarea>
+						</tui-textfield>
+					}
+				}
+			</section>
+	    </div>
+    `
+})
+export class SampleContainer {
+    @Input() label: string = '';
+
+	index = signal<number>(0);
+
+	@Input() html : string = "";
+	@Input() css : string = "";
+	@Input() ts : string = "";
+
+	@Input() enableStyle : boolean = false;
+	@Input() enableTypescript : boolean = false;
+
+	@ContentChild('preview', { static: false }) preview: TemplateRef<any> | undefined;
+}
--- projects/ez-ui-demo/src/app/pages/input/textInput.ts
+++ projects/ez-ui-demo/src/app/pages/input/textInput.ts
@@ -2,35 +2,72 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { EzUITextInput } from 'EzUI';
+import { SampleContainer } from "../../common/samplecontainer";
 
 @Component({
     selector: 'app-text-input',
     imports: [
     FormsModule,
     CommonModule,
-	EzUITextInput
+    EzUITextInput,
+    SampleContainer
 ],
     template: `
-	<span>Simple</span>
-	<ezui-textinput />
+	<app-samplecontainer label="Simple" html='<ezui-textinput />'>
+		<ng-template #preview>
+			<ezui-textinput />
+		</ng-template>
+	</app-samplecontainer>
 
-	<span>Label</span>
-	<ezui-textinput label="Simple Label" />
+	<app-samplecontainer label="Simple Label" html='<ezui-textinput label="Simple Label" />'>
+		<ng-template #preview>
+			<ezui-textinput label="Simple Label" />
+		</ng-template>
+	</app-samplecontainer>
 
-	<span>Label and Icon</span>
-	<ezui-textinput label="Label with icon" icon="square-pen" />
+	<app-samplecontainer label="Label and Icon" html='<ezui-textinput label="Label with icon" icon="square-pen" />'>
+		<ng-template #preview>
+			<ezui-textinput label="Label with icon" icon="square-pen" />
+		</ng-template>
+	</app-samplecontainer>
 
-	<span>Binding</span>
-	<ezui-textinput [(value)]="binding" label="Binding" />
-	<span>You wrote: {{binding()}}</span>
+	<app-samplecontainer label="Label and Icon" html='<ezui-textinput label="Label with icon" icon="square-pen" />'>
+		<ng-template #preview>
+			<ezui-textinput label="Label with icon" icon="square-pen" />
+		</ng-template>
+	</app-samplecontainer>
 
-	<span>Sizes</span>
-	<ezui-textinput label="Small" icon="square-pen" size="s"/>
-	<ezui-textinput label="Medium" icon="square-pen" size="m"/>
-	<ezui-textinput label="Large" icon="square-pen" size="l"/>
+	<app-samplecontainer
+		label="Binding"
+		html='<ezui-textinput [(value)]="binding" label="Binding" />'
+		[enableTypescript]="true"
+		ts='binding = signal<string>("");'
+	>
+		<ng-template #preview>
+			<ezui-textinput [(value)]="binding" label="Binding" />
+			<span>You wrote: {{binding()}}</span>
+		</ng-template>
+	</app-samplecontainer>
 
-	<span>Disabled</span>
-	<ezui-textinput label="Disabled" icon="square-pen" [disabled]="true"/>
+	<app-samplecontainer
+		label="Sizes"
+		html='<ezui-textinput label="Small" icon="square-pen" size="s"/>
+<ezui-textinput label="Medium" icon="square-pen" size="m"/>
+<ezui-textinput label="Large" icon="square-pen" size="l"/>
+		'
+	>
+		<ng-template #preview>
+			<ezui-textinput label="Small" icon="square-pen" size="s"/>
+			<ezui-textinput label="Medium" icon="square-pen" size="m"/>
+			<ezui-textinput label="Large" icon="square-pen" size="l"/>
+		</ng-template>
+	</app-samplecontainer>
+
+	<app-samplecontainer label="Disabled" html='<ezui-textinput label="Disabled" icon="square-pen" [disabled]="true"/>'>
+		<ng-template #preview>
+			<ezui-textinput label="Disabled" icon="square-pen" [disabled]="true"/>
+		</ng-template>
+	</app-samplecontainer>
     `,
     host: {
         class: 'base-view'
