fd36de74fde376aaf5c601cb6430183f31f12e1c
Kristian Skov <kris701kj@gmail.com>
2026-08-04 10:33:42 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/kris701/EzUI/commit/b05eff47bd7a9e4e18a191a347f6705d8ee04021
    GitHub Actions build: https://github.com/kris701/EzUI/actions/runs/30901025234

---
+++ chunk-DKP0oKOr.js
@@ -0,0 +1,49 @@
+import{J as ie,S as Mn,U as _,W as bn,dt as x,f as I,ft as y,i as B9,p as J,q as g,st as v,y as Ln}from"./main-2RJYXX4A.js";import{t as Y}from"./chunk-B8xojADt.js";function d(t,m){if(t&1&&J(0,`ezui-menubar`,3),t&2)x(`items`,g().items)}function f(t,m){if(t&1&&J(0,`ezui-menubar`,3),t&2)x(`items`,g().items2)}var z=[{path:`menubar`,component:class t{items=[{label:`Button 1`,icon:`check`,command:()=>alert(`button 1 click`)},{label:`Some longer value name here`,icon:`x`,command:()=>alert(`button 2 click`)},{label:`Button 3`,icon:`plus`,command:()=>alert(`button 3 click`)}];items2=[{label:`Button 1`,icon:`check`,items:[{label:`Opt 1`,command:()=>alert(`opt 1 click`)},{label:`Opt 2`,command:()=>alert(`opt 2 click`)}]},{label:`Button 2`,icon:`x`,items:[{label:`Opt 1`,command:()=>alert(`opt 1 click`)},{label:`Opt 2`,command:()=>alert(`opt 2 click`),items:[{label:`Opt 3`,command:()=>alert(`opt 3 click`)}]}]}];static ɵfac=function(e){return new(e||t)};static ɵcmp=I({type:t,selectors:[[`app-menubar`]],hostAttrs:[1,`base-view`],decls:6,vars:2,consts:[[`preview`,``],[`label`,`Simple`,`html`,`<ezui-menubar [items]="items"/>`,`ts`,`items : MenuBarItem[] = [
+	{
+		label: 'Button 1',
+		icon: 'check'
+	} as MenuBarItem,
+	{
+		label: 'Some longer value name here',
+		icon: 'x'
+	} as MenuBarItem,
+	{
+		label: 'Button 3',
+		icon: 'plus'
+	} as MenuBarItem,
+]`,3,`enableTypescript`],[`label`,`Hierarchical`,`html`,`<ezui-menubar [items]="items"/>`,`ts`,`items2 : MenuBarItem[] = [
+	{
+		label: 'Button 1',
+		icon: 'check',
+		items: [
+			{
+				label: 'Opt 1',
+				command: () => alert('opt 1 click')
+			} as MenuBarItem,
+			{
+				label: 'Opt 2',
+				command: () => alert('opt 2 click')
+			} as MenuBarItem
+		] as MenuBarItem[]
+	} as MenuBarItem,
+	{
+		label: 'Button 2',
+		icon: 'x',
+		items: [
+			{
+				label: 'Opt 1',
+				command: () => alert('opt 1 click')
+			} as MenuBarItem,
+			{
+				label: 'Opt 2',
+				command: () => alert('opt 2 click'),
+				items: [
+					{
+						label: 'Opt 3',
+						command: () => alert('opt 3 click')
+					} as MenuBarItem
+				] as MenuBarItem[]
+			} as MenuBarItem,
+		] as MenuBarItem[]
+	} as MenuBarItem,
+]`,3,`enableTypescript`],[3,`items`]],template:function(e,y$1){e&1&&(y(0,`app-samplecontainer`,1),ie(1,d,1,1,`ng-template`,null,0,Mn),_(),y(3,`app-samplecontainer`,2),ie(4,f,1,1,`ng-template`,null,0,Mn),_()),e&2&&(x(`enableTypescript`,!0),v(3),x(`enableTypescript`,!0))},dependencies:[Ln,bn,B9,Y],encapsulation:2})}}];export{z as default};
